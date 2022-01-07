dec95c1cd16896feb120aa92fdc7e4ee428c4567
snowfallen <bondarenkoov1998@gmail.com>
2022-01-07 19:49:51 +0100

    add file

---
+++ PostController.js
@@ -0,0 +1,49 @@
+import Post from "./Post.js"
+
+class PostController {
+    async create(req , res,){
+        try{
+            const {author , title , content , picture} = req.body
+            const post = await Post.create({author , title , content , picture})
+            res.json(post)
+        }catch(e){
+            res.status(500).json(e)
+        }
+    }
+    async getAll(req , res){
+        try{
+            const posts = await Post.find();
+            return res.json(posts);
+        }catch(e){
+            res.status(500).json(e)
+        }
+    }
+    async getOne(req , res){
+        try{
+            const {id} = req.params
+            if(!id){
+                res.status(400).json({message:"ID dont inputet"})
+            }
+            const post = await Post.findById(id)
+            return res.json(post)
+        }catch(e){
+            res.status(500).json(e)
+        }
+    }
+    async update(req , res){
+        try{
+
+        }catch(e){
+            res.status(500).json(e)
+        }
+    }
+    async delete(req , res){
+        try{
+
+        }catch(e){
+            res.status(500).json(e)
+        }
+    }
+}
+
+export default new PostController();
---
+++ index.js
@@ -0,0 +1,23 @@
+import express from "express";
+import mongoose from "mongoose";
+import router from "./router.js";
+
+const PORT = 7000;
+
+const app = express();
+
+const DB_URL=`mongodb+srv://user:1234@cluster0.j5mem.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+
+app.use(express.json())
+app.use('/api', router)
+
+
+async function startApp(){
+    try{
+        await mongoose.connect(DB_URL)
+        app.listen(PORT, ()=>console.log("Server working on port " + PORT))
+    }catch(e){
+        console.log(e);
+    }
+}
+startApp()
